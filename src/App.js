cf2a4a1ad8c8313b28f046832dae12c5205e51e2
Nguyễn Danh Thái <47393239+tdpndthai@users.noreply.github.com>
2020-05-01 23:40:50 +0700

    init project with class components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Navbar from "./components/layout/Navbar";
+import UserItem from "./user/UserItem";
+import Users from "./user/Users";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  //foo =()=>'bars'; //đây là 1 phương thức của lớp
+  render(){
+    const name ='danh thai';
+    const loading = false;
+    // if (loading){
+    //     return  <h3>Loading...</h3>
+    // }
+    const number=[1,2,3,4]
+    return (
+        <div className="App">
+          {/*<h1>Hello React {name.toUpperCase()}</h1>*/}
+          {/*/!*<h2>Hello {this.foo()}</h2>*!/*/}
+          {/*  {loading ? <h3>Loading...</h3>: <h3>Hello {name}</h3>}*/}
+          <Navbar></Navbar>
+            {/*<UserItem></UserItem>*/}
+            <div className="container">
+                <Users></Users>
+            </div>
+        </div>
+    );
+    //return React.createElement('div',{className:'App'},React.createElement('h1',null,'hello react'))
+  }
+
 }
 
 export default App;
